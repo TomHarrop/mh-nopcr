e622a61526a18b126fd436012107ca93214b1aaf
Tom Harrop <twharrop@gmail.com>
2017-05-16 10:56:39 +1200

    weird data.table error, have to work around in ggplot call

--- src/r/plot_kmer_distribution.R
+++ src/r/plot_kmer_distribution.R
@@ -10,8 +10,8 @@ library(scales)
 # IO files
 command.args <- commandArgs(trailingOnly = TRUE)
 # command.args <- c(
-#     "--fq", "output/bbnorm/ASW_normalised.fastq.gz",
-#     "-r", "output/bbnorm/kmer_distribution_plot.pdf")
+#  "--fq", "output/bbnorm/Mh_normalised.fastq.gz",
+#  "-r", "output/bbnorm/kmer_distribution_plot.pdf")
 parsed.args <- argparsR::ParseArguments(
     accepted.switches = list(
         "input_fq" = "--fq",
@@ -39,11 +39,6 @@ peaks <- fread(peaks_data_file)
 before_data[, type := "Raw"]
 after_data[, type := "Normalised"]
 
-combined_data <- rbind(before_data, after_data)
-
-# arrange plot
-combined_data[, type := factor(type, levels = c("Raw", "Normalised"))]
-
 # hlines
 mincov <- peaks[1, V1]
 p1 <- peaks[1, V2]
@@ -52,13 +47,16 @@ maxcov <- peaks[2, V3]
 
 # plot
 Set1 <- RColorBrewer::brewer.pal(9, "Set1")
-kmer_plot <- ggplot(combined_data, aes(x = `#Depth`, y = Unique_Kmers,
+kmer_plot <- ggplot(mapping = aes(x = `#Depth`, y = Unique_Kmers,
                           colour = type)) +
     theme_minimal() +
     theme(legend.position = c(5/6, 2/4)) +
     geom_vline(xintercept = c(mincov, p1, p2, maxcov),
                colour = Set1[9]) +
-    geom_path(alpha = 0.75) +
+    geom_path(data = before_data,
+              alpha = 0.75) +
+    geom_path(data = after_data,
+              alpha = 0.75) +
     scale_colour_brewer(palette = "Set1",
                         guide = guide_legend(title = NULL)) +
     scale_y_continuous(
